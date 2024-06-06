542db11a37a0599fdf7541843256c9fdef343d34
dtiDigital <dtiDigital@DPCJULIABGS>
2024-06-06 14:22:32 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Repositorios;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Configurar o DbContext com SQL Server
+            builder.Services.AddDbContext<SistemaTarefasDBContex>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
+
+            // Registrar repositórios
+            builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
